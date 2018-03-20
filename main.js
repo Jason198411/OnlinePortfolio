9a1f637b7098adcbf7e3a382a6b290c5c41a841f
Jason198411 <jasonwang198411@gmail.com>
2018-03-21 00:21:40 +1100

    update

--- main.js
+++ main.js
@@ -5,40 +5,40 @@ var coll = {
     "projectName": "Responsive Layout Demo",
     "img" : "imges/responsive.png",
     "lable": [],
-    "linkDemo": "https://jason198411.github.io/responsive/",
-    "linkSource": "https://github.com/Jason198411/responsive",
+    "linkDemo": "https://jason198411.github.io/Responsive/",
+    "linkSource": "https://github.com/Jason198411/Responsive",
     "description": "In this project I built a responsive, mobile-first layout using HTML and CSS. The layout could demonstrate an understanding of responsive design by adjusting to accommodate small, medium, and large screen sizes. I wrote CSS to style the page for a small mobile device first. Then used min-width media queries, I added breakpoints to adjust the layout for wider tablet and desktop screens"
   },
   "photo" :{    
     "projectName": "Photo Gallery Demo",
     "img" : "imges/photo-gallery.png",
     "lable": [{"lb":"Javascript", "lbClass":"badge badge-secondary"},{"lb":"jQuery", "lbClass":"badge badge-success"}],
-    "linkDemo": "https://jason198411.github.io/photo/",
-    "linkSource": "https://github.com/Jason198411/photo",
+    "linkDemo": "https://jason198411.github.io/Photo/",
+    "linkSource": "https://github.com/Jason198411/Photo",
     "description": "In this project, I created an interactive photo gallery using JavaScript and jQuery Plugins. Thumbnails and photos were provided with descriptions. At the top of the page have a search area where photos will hide and show depending on user input. When the user clicks on a thumbnail, the photo will display in a lightbox. There should be a back and previous arrows to cycle through photos."
   },
   "game" :{    
     "projectName": "Game Demo",
     "img" : "imges/game.png",
     "lable": [{"lb":"Javascript", "lbClass":"badge badge-secondary"}],
-    "linkDemo": "https://jason198411.github.io/game/",
-    "linkSource": "https://github.com/Jason198411/game",
+    "linkDemo": "https://jason198411.github.io/Game/",
+    "linkSource": "https://github.com/Jason198411/Game",
     "description": "In this project I created a browser version of “Wheel of Success”, a word guessing game where players will click letters from an onscreen keyboard to try to guess a random phrase. I used Javascript to create an array of phrases and wrote functions to choose a random phrase from that array and put those letters onto the gameboard. Each time the player guesses a letter, if the letter is in the phrase, update the game board with the chosen letters. Otherwise, lose one chance, If the player completes the phrase before they run out of guesses, a winning screen will display. If the player guesses incorrectly 5 times, a losing screen will display."
   },
   "dashboard" :{    
     "projectName": "Dashboard Demo",
     "img" : "imges/dashboard.png",
     "lable": [{"lb":"Javascript", "lbClass":"badge badge-secondary"}],
-    "linkDemo": "https://jason198411.github.io/dashboard/",
-    "linkSource": "https://github.com/Jason198411/dashboard",
+    "linkDemo": "https://jason198411.github.io/Dashboard/",
+    "linkSource": "https://github.com/Jason198411/Dashboard",
     "description": "In this project, I took a mockup and a few icons and built a beautiful, web dashboard complete with JavaScript-driven charts and graphs. I took the design and created the HTML, CSS and JavaScript functionality for this one page. I didn’t create other pages, the data is the makeup."
   },
   "employee" :{    
     "projectName": "Employee Directory Demo",
     "img" : "imges/employee.png",
     "lable": [{"lb":"Javascript", "lbClass":"badge badge-secondary"},{"lb":"API", "lbClass":"badge badge-warning"}],
-    "linkDemo": "https://jason198411.github.io/employee/",
-    "linkSource": "https://github.com/Jason198411/employee",
+    "linkDemo": "https://jason198411.github.io/Employee/",
+    "linkSource": "https://github.com/Jason198411/Employee",
     "description": "In this project, I used the Random User Generator API to grab information for 12 random “employees,” and use that data to build a prototype for an Awesome Startup employee directory. I requested a JSON object from the API and parse the data so that 12 employees are listed in a grid with their thumbnail image, full name, email, and location. Clicking the employee’s image or name will open a modal window with more detailed information. "
   }, 
   "flickr" :{    
@@ -46,7 +46,7 @@ var coll = {
     "img" : "imges/flickr.png",
     "lable": [{"lb":"Javascript", "lbClass":"badge badge-secondary"},{"lb":"React","lbClass":"badge badge-info"},{"lb":"API", "lbClass":"badge badge-warning"}],
     "linkDemo": "",
-    "linkSource": "https://github.com/Jason198411/flickr",
+    "linkSource": "https://github.com/Jason198411/Flickr",
     "description": "In this project, I created an image gallery using React and the Flickr API. I built the gallery components, wrote the CSS and set up routing. The page include a Search link that includes a search field to let users search for photos. Under the search field, there are 3 links that return a list of photos matching the criteria I used React Router to set up routes for each navigation link in the directory app."
   }   
 }
@@ -107,5 +107,7 @@ $( window ).on( "load", function() {setTimeout(() =>{
   $("body").removeClass("noScroll");
   $(".loading").hide();
   $(".navbar").addClass("fixed-top");
+  $('.lead').slideDown(1000);
 }, 2000)});
 
+//$('.lead').animate({'margin-top': '50px'}, 5000);
